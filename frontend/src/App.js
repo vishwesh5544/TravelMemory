7c71323373c6961ed7b88d2459b255805bf36bf0
vishwesh5544 <vishweshshukla20@gmail.com>
2024-09-21 16:38:33 +0530

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import ExperienceDetails from './components/pages/ExperienceDetails';
 
 const router = createBrowserRouter([
   {
-    path: '/frontend',
+    path: '/',
     element: <Home></Home>
   },
   {
